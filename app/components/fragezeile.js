6de843a8b772f8439beb38ce482b477be61855fd
kindermann <kindermann@htl.rennweg.at>
2017-11-16 14:50:53 +0100

    First Commit

---
+++ app/components/fragezeile.js
@@ -0,0 +1,11 @@
+app.component("fragezeile", {
+    templateUrl: "components/fragezeile.html",
+    bindings: {
+        radio1:"@?",
+        radio2:"@?",
+        radio3:"@?",
+        radio4:"@?",
+        radio5:"@?",
+        frage:"@"
+    }
+});
